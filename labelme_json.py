03acc96200a51bfe85784ca09b1f5655ad2cd37e
taashi-s <x7286x_ayk@yahoo.co.jp>
2018-09-10 20:33:20 +0900

    Add main script

---
+++ labelme_json.py
@@ -0,0 +1,86 @@
+import os
+import json
+import base64
+
+class LabelmeJSON():
+    KEY_FLAGS = 'flags'
+    KEY_SHAPES = 'shapes'
+    KEY_L_COLOR = 'lineColor'
+    KEY_F_COLOR = 'fillColor'
+    KEY_IMG_PATH = 'imagePath'
+    KEY_IMG = 'imageData'
+
+    DEFAULT_L_COLOR = [255, 0, 255, 128]
+    DEFAULT_F_COLOR = [85, 170, 127, 128]
+
+
+    def __init__(self, img_path, flags=None, shapes=None, lineColor=None, fillColor=None):
+
+        self.__img_path = img_path
+
+        self.__json = self.__get_empty_json()
+
+        if flags is not None:
+            self.__json[self.KEY_FLAGS] = flags
+        if shapes is not None:
+            self.__json[self.KEY_SHAPES] = [shape.get() for shape in shapes]
+        if lineColor is not None:
+            self.__json[self.KEY_L_COLOR] = lineColor
+        if fillColor is not None:
+            self.__json[self.KEY_F_COLOR] = fillColor
+        if os.path.exists(img_path):
+            self.__json[self.KEY_IMG_PATH] = os.path.basename(img_path)
+            with open(self.__img_path, 'rb') as f:
+                img_base64 = base64.encodestring(f.read())
+            self.__json[self.KEY_IMG] = img_base64.decode('utf8')
+
+
+    def __get_empty_json(self):
+        j = {}
+        j[self.KEY_IMG_PATH] = ''
+        j[self.KEY_IMG] = ''
+        j[self.KEY_FLAGS] = {}
+        j[self.KEY_L_COLOR] = self.DEFAULT_L_COLOR
+        j[self.KEY_F_COLOR] = self.DEFAULT_F_COLOR
+        j[self.KEY_SHAPES] = []
+        return j
+
+
+    def get(self):
+        return self.__json
+
+
+    def dumps(self):
+        return json.dumps(self.get())
+
+
+    def dump(self, file_path=None):
+        if file_path is None:
+            dir_path = os.path.dirname(self.__img_path)
+            file_name, _ = os.path.splitext(os.path.basename(self.__img_path))
+            file_path = os.path.join(dir_path, file_name + '.json')
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        with open(file_path, 'w') as f:
+            json.dump(self.get(), f, indent=2)
+        return file_path
+
+
+class LabelmeJSONShape():
+    KEY_LABEL = 'label'
+    KEY_L_COLOR = 'line_color'
+    KEY_F_COLOR = 'fill_color'
+    KEY_POINTS = 'points'
+
+    def __init__(self, label, line_color=None, fill_color=None, points=None):
+        self.__shape = {}
+        self.__shape[self.KEY_LABEL] = label
+        self.__shape[self.KEY_L_COLOR] = line_color
+        self.__shape[self.KEY_F_COLOR] = fill_color
+        self.__shape[self.KEY_POINTS] = []
+        if points is not None:
+            self.__shape[self.KEY_POINTS] = points
+
+
+    def get(self):
+        return self.__shape
---
+++ seg_to_labelme_json.py
@@ -0,0 +1,128 @@
+import os
+import numpy as np
+import cv2
+import glob
+from tqdm import tqdm
+
+from labelme_json import LabelmeJSON, LabelmeJSONShape
+
+
+BASE_DIR = os.path.join('.', 'data')
+ORG_DIR = os.path.join(BASE_DIR, 'images')
+MSK_DIR = os.path.join(BASE_DIR, 'masks')
+IMAGE_EXTENTAIONS = ['.png', '.jpg', '.jpeg']
+
+NONE_ID = -1
+USE_CHAIN_APPROX = cv2.CHAIN_APPROX_TC89_KCOS
+#USE_CHAIN_APPROX = cv2.CHAIN_APPROX_NONE
+#USE_CHAIN_APPROX = cv2.CHAIN_APPROX_SIMPLE
+#USE_CHAIN_APPROX = cv2.CHAIN_APPROX_TC89_L1
+
+ORG_IMG_NAME_INDEX = 0
+LABEL_NAME_INDEX = 1
+
+
+def get_first_contor_id(hierarchy):
+    first_contor_index = 0
+
+    for k ,h in enumerate(hierarchy):
+        _, prev_id, _, pparent_id = h
+        if prev_id == NONE_ID and pparent_id ==  NONE_ID:
+            first_contor_index = k
+            break
+
+    return first_contor_index
+
+
+def follow_contor(contor_id, hierarchy, contours, disp_prefix='- ', is_inside=False):
+    contor_info = hierarchy[contor_id]
+    next_id, prev_id, child_id, parent_id = contor_info
+
+    #print(disp_prefix, ' %03d : ' % contor_id, contor_info)
+
+    contour = contours[contor_id]
+    cnts = []
+    child_ids = []
+    child_contours = []
+    if child_id != NONE_ID:
+        child_ids, child_contours, child_cnts = follow_contor(child_id, hierarchy, contours
+                                                              , disp_prefix[:-2] + '     - '
+                                                              , is_inside=not is_inside)
+        cnts += child_cnts
+
+    next_ids = []
+    next_contours = []
+    if next_id != NONE_ID:
+        next_ids, next_contours, next_cnts = follow_contor(next_id, hierarchy, contours, disp_prefix)
+        cnts += next_cnts
+
+    if not is_inside:
+        cnt = np.concatenate([contour] + child_contours)
+        cnt = np.squeeze(cnt, axis=1)
+        cnts.append(cnt.tolist())
+
+    same_tier_ids = [contor_id] + next_ids
+    same_tier_contours = [contour] + next_contours
+    return same_tier_ids, same_tier_contours, cnts
+
+
+def get_points_from_mask_img(mask_file):
+    mask_img = cv2.imread(mask_file)
+    mask_img_gray = cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY)
+    _, contours, hierarchy = cv2.findContours(mask_img_gray, cv2.RETR_CCOMP, USE_CHAIN_APPROX)
+    if hierarchy is None:
+        return []
+    if len(hierarchy) < 1:
+        return []
+    contor_id = get_first_contor_id(hierarchy[0])
+    _, _, masks = follow_contor(contor_id, hierarchy[0], contours)
+    return masks[0]
+
+
+def get_image_ext_pathes(base_path):
+    pathes = []
+    for ext in IMAGE_EXTENTAIONS:
+        pathes += glob.glob(base_path + ext)
+    return pathes
+
+
+def save_shape_to_json(original_name, shapes):
+    org_img_pathes = get_image_ext_pathes(os.path.join(ORG_DIR, original_name))
+    created_count = 0
+    if len(org_img_pathes) > 0:
+        labelme_json = LabelmeJSON(org_img_pathes[0], shapes=shapes)
+        labelme_json.dump()
+        created_count = 1
+    return created_count
+
+
+if __name__ == '__main__':
+    mask_files = get_image_ext_pathes(os.path.join(MSK_DIR, '*'))
+    mask_files.sort()
+
+    original_name = ''
+    shapes = []
+    created_json_count = 0
+
+    pbar = tqdm(total=len(mask_files), desc="Transform mask image", unit=" files")
+    for mask_file in mask_files:
+        basename = os.path.basename(mask_file)
+        name_parts = basename.split('_')
+
+        if len(name_parts) < 3:
+            continue
+
+        if original_name == '':
+            original_name = name_parts[ORG_IMG_NAME_INDEX]
+
+        if original_name != name_parts[ORG_IMG_NAME_INDEX]:
+            created_json_count += save_shape_to_json(original_name, shapes)
+            original_name = name_parts[ORG_IMG_NAME_INDEX]
+            shapes = []
+        points = get_points_from_mask_img(mask_file)
+        shapes.append(LabelmeJSONShape(name_parts[LABEL_NAME_INDEX], points=points))
+        pbar.update(1)
+    created_json_count += save_shape_to_json(original_name, shapes)
+    pbar.close()
+
+    print('### Created JSON : ', created_json_count, ' files')
